8bab35a365d9cbb45c1081dc842a99f928f46ffc
mrohith29 <mariyalarohith29@gmail.com>
2023-06-01 21:33:28 +0530

    done

--- priority_queue.c
+++ priority_queue.c
@@ -1,106 +1,95 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 #define queue_size 3
-struct pqueue
-{
+
+struct pqueue {
     char job;
     int pr;
-}e[queue_size];
+} e[queue_size];
 
-int front=-1,rear=-1;
+int front = -1, rear = -1;
 
 void enqueue(char element, int p)
 {
-    if(rear==queue_size)
-    {
+    if (rear == queue_size - 1) {
         printf("queue is full\n");
-    }
-    else
-    {
+    } else {
         rear++;
-        e[rear].job=element;
-        e[rear].pr=p;
+        e[rear].job = element;
+        e[rear].pr = p;
     }
 }
-
 
 void dequeue()
 {
-    if(front==rear)
-    {
-        printf("\nqueue is empty");
-    }
-    else{
+    if (front == rear) {
+        printf("\nqueue is empty\n");
+    } else {
         front++;
-        // printf("")
+        // Shift elements in the array
+        for (int i = front; i <= rear; i++) {
+            e[i] = e[i + 1];
+        }
+        rear--;
     }
 }
 
-
 void sort()
 {
-    int i,j;
+    int i, j;
     struct pqueue temp;
-    for(i=front+1;i<=rear;i++)
-    {
-        for(j=i+1;j<=rear;j++)
-        {
-            if(e[i].pr<e[j].pr)
-            {
-                temp=e[i];
-                e[i]=e[j];
-                e[j]=temp;
+    for (i = front + 1; i <= rear; i++) {
+        for (j = i + 1; j <= rear; j++) {
+            if (e[i].pr < e[j].pr) {
+                temp = e[i];
+                e[i] = e[j];
+                e[j] = temp;
             }
         }
     }
 }
 
-
 void display()
 {
     int i;
-    if(front==rear)
-    {
+    if (front == rear) {
         printf("\nqueue is empty\n");
-    }
-    else{
-        printf("  job name\tpriority\n");
-        for(i=front+1;i<=rear;i++)
-        {
-            printf("  %c\t-\t%d",e[i].job,e[i].pr);
+    } else {
+        printf("  job name\t\tpriority\n");
+        for (i = front + 1; i <= rear; i++) {
+            printf("  %c\t\t%d\n", e[i].job, e[i].pr);
         }
     }
 }
 
-
-
-void main()
+int main()
 {
-    int p,ch;
+    int p, ch;
     char element;
-    while(1)
-    {
-        scanf("%d",&ch);
-        switch(ch)
-        {
+    while (1) {
+        scanf("%d", &ch);
+        switch (ch) {
             case 1:
-            scanf("%c %d",&element,&p);
-            enqueue(element,p);
-            sort();
-            break;
-            
+                scanf(" %c %d", &element, &p);
+                enqueue(element, p);
+                sort();
+                break;
+
             case 2:
-            dequeue();
-            break;
-            
+                dequeue();
+                break;
+
             case 3:
-            display();
-            break;
-            
+                display();
+                break;
+
             case 4:
-            exit(0);
+                exit(0);
+
+            default:
+                printf("Invalid choice\n");
         }
     }
+    return 0;
 }
