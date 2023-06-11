00dbee209ca66d530e6dcbb777215ef946f2073b
mrohith29 <mariyalarohith29@gmail.com>
2023-06-11 20:59:54 +0530

    done.

--- insertionsort.c
+++ insertionsort.c
@@ -1,23 +1,28 @@
 #include<stdio.h>
 int main()
 {
-    int i,j,n,a[10],b[10];
+    int i,n,a[20];
+    printf("Enter the number of numbers to be entered\n");
     scanf("%d",&n);
     for(i=0;i<n;i++)
-    scanf("%d",&a[i]);
-    printf("Unsorted List\n");
-    for(i=0;i<n;i++)
-    printf("%d ",a[i]);
-    printf("\nINSERTION SORT\nSorted List\n");
+    {
+        scanf("%d",&a[i]);
+    }
     for(i=1;i<n;i++)
     {
-        int temp=a[i];
-        for(j=i;j>0,a[j-1]>temp;j--)
+        int temp,j;
+        temp=a[i];
+        j=i;
+        while ((j>0) && (a[j-1]>temp))
         {
             a[j]=a[j-1];
+            j--;
         }
-        a[j]=temp;
+        a[j]=temp;    
     }
-    for(i=0;i<n;i++)
-    printf("%d ",a[i]);
+    for ( i = 0; i < n; i++)
+    {
+        printf("%d  ",a[i]);
+    }
+    
 }
