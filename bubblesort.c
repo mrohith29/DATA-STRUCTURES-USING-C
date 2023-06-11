f3c8f4c5c47fe52374ea0ac05636a30437ab9146
mrohith29 <mariyalarohith29@gmail.com>
2023-06-11 16:30:52 +0530

    done bubblesort

---
+++ bubblesort.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,temp,a[20],n;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n-i-1;j++)
+        {
+            if(a[j]>a[j+1])
+            {
+                temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+            }
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        printf("%d\t",a[i]);
+    }
+}
