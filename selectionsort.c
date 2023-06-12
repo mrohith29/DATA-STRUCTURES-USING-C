cb899d7c50264d6a97ae98560f25e0f6e13c1817
mrohith29 <mariyalarohith29@gmail.com>
2023-06-12 18:34:51 +0530

    done.

--- selectionsort.c
+++ selectionsort.c
@@ -1,9 +1,32 @@
 #include<stdio.h>
 int main()
 {
-    int i,n,a[20];
+    int i,n,min,temp,a[20];
     printf("Enter the number of elements to ne sorted\n");
     scanf("%d",&n);
     for(i=0;i<n;i++)
-    printf("%d ",a[i]);
+    scanf("%d",&a[i]);
+    for(i=0;i<n;i++)
+    {
+        min=i;
+        for(int j=i+1;j<n;j++)
+        {
+            if(a[j]<a[min])
+            {
+                min=j;
+            }
+        }
+
+        if(min!=i)
+        {
+            temp=a[min];
+            a[min]=a[i];
+            a[i]=temp;       
+        }
+    }
+    printf("sorted array is\n");
+    for(i=0;i<n;i++)
+    {
+        printf("%d  ",a[i]);
+    }
 }
