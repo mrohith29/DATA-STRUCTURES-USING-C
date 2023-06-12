64975bdb2e0fa4248654f54500b84e066b7162fa
mrohith29 <mariyalarohith29@gmail.com>
2023-06-12 18:33:16 +0530

    done.

---
+++ selectionsort.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,a[20];
+    printf("Enter the number of elements to ne sorted\n");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    printf("%d ",a[i]);
+}
