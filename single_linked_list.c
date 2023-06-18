9e829e41979ce8aef969eaa3519eea15f9aea881
mrohith29 <mariyalarohith29@gmail.com>
2023-06-18 10:13:49 +0530

    going..

--- single_linked_list.c
+++ single_linked_list.c
@@ -5,12 +5,43 @@ struct node
     int data;
     struct node *next;
 };
+
+void linkedlisttraversal(struct node *ptr)
+{
+    while(ptr != NULL)
+    {
+        printf("Element: %d\n",ptr->data);
+        ptr = ptr->next;
+    }
+}
+
+void insert_at_beginning(struct node *head,int data)
+{
+    printf("Enter the data to enter before at the beginning");
+    scanf(" %d ", &head->data);
+    head->next = head;
+
+}
 int main()
 {
+    int option;
     struct node *head;
-    struct node *second;
-    struct node *third;
-    struct node *fourth;
+    struct node *newnode;
+    
+    newnode=(struct node*)malloc(sizeof(struct node));
+    printf("Enter the element in the new node.\n");
+    scanf("%d", &newnode->data);
+    newnode->next = NULL;  
 
-    printf("%d",sizeof(head));
+    printf("Select your option:\n\
+    1. Insert at the beginning of the element\n\
+    2. Insert before the element\n\
+    3. Insert after the element\n");
+
+    scanf("%d",option);
+    switch(option)
+    {
+        case 1: 
+    }
+
 }
