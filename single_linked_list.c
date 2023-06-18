798866c5fde28d94229fbae99eb0745172a124cf
mrohith29 <mariyalarohith29@gmail.com>
2023-06-18 11:34:20 +0530

    going..

--- single_linked_list.c
+++ single_linked_list.c
@@ -1,5 +1,5 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 struct node
 {
     int data;
@@ -8,50 +8,76 @@ struct node
 
 void linkedlisttraversal(struct node *ptr)
 {
-    while(ptr != NULL)
+    while (ptr != NULL)
     {
-        printf("Element: %d\n",ptr->data);
+        printf("Element: %d\n", ptr->data);
         ptr = ptr->next;
     }
 }
 
-struct node* insert_at_beginning(struct node *head)
+struct node *insert_at_beginning(struct node *head)
 {
-    // int info;
-    struct node *ptr = (struct node*)malloc(sizeof(struct node));
-    printf("Enter the data to enter before at the beginning");
-    
-    scanf(" %d ", &ptr->data);
+    struct node *ptr = (struct node *)malloc(sizeof(struct node));
+    printf("Enter the data to enter before at the beginning\n");
+    scanf("%d", &ptr->data);
     ptr->next = head;
-    return head;
+    return ptr;
+    // while(ptr!=NULL)
+    // {
+    //     printf("%d\n",ptr->data);
+    //     ptr=ptr->next;
+    // }
 }
+
+struct node *insert_at_end(struct node *head)
+{
+    struct node *ptr = (struct node *)malloc(sizeof(struct node));
+    printf("Enter the element needed to insert at the end\n");
+    scanf("%d", &ptr->data);
+    ptr->next = NULL;
+    struct node *p = head;
+    while (p->next != NULL)
+    {
+        p = p->next;
+    }
+    p->next = ptr;
+    return p;
+}
+
 int main()
 {
     int option;
     struct node *head;
-    
-    head= (struct node*)malloc(sizeof(struct node));
+
+    head = (struct node *)malloc(sizeof(struct node));
     printf("Enter the element in the \"head\".\n");
     scanf("%d", &head->data);
-    head->next = NULL;  
+    head->next = NULL;
 
     // printf("%d\n",head->data);
-    
-    printf("Select your option:\n\
+
+    while (1)
+    {
+        printf("Select your option:\n\
+    -1. Display the list\n\
     1. Insert at the beginning of the element\n\
     2. Insert before the element\n\
-    3. Insert after the element\n");
-    while(1)
-    {
-    scanf("%d",option);
-    switch(option)
-    {
+    3. Insert after the element\n\
+    4. Insert at the end\n\
+    0. exit\n");
+        scanf("%d", &option);
+        switch (option)
+        {
+        case 0:
+            exit(0);
         case 1:
-         insert_at_beginning(head);
-        break;
-         case 2:
-         linkedlisttraversal(head);
-        break;
-    }
+            head = insert_at_beginning(head);
+            break;
+        case -1:
+            linkedlisttraversal(head);
+            break;
+        case 4:
+            head = insert_at_end(head);
+        }
     }
 }
